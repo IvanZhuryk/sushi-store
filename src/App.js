b19184874673bed9ad4fdc2a9ea75b5977733ca8
Zhuryk Ivan <ivannom777@gmail.com>
2023-03-01 15:11:32 +0200

    fixdata

--- src/App.js
+++ src/App.js
@@ -29,14 +29,14 @@ import { addDrinks } from './redux/slices/drinksSlice';
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyBFOW8mRyaJ33zcOY6ih33vxUF0PFE-Wmw',
+  authDomain: 'sushi-2bd73.firebaseapp.com',
+  databaseURL: 'https://sushi-2bd73-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: "sushi-2bd73",
+  storageBucket: 'sushi-2bd73.appspot.com',
+  messagingSenderId: '716604920724',
+  appId: '1:716604920724:web:5d216d9d5e8aa3015ab581',
+  measurementId: 'G-R1CL4FLHWG',
 };
 
 const app = initializeApp(firebaseConfig);
